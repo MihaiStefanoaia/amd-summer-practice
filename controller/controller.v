96c8edcc9d4499904b775f6c69a12e46a553e2f2
Stefanoaia Mihai <miahistefanoaia1102@gmail.com>
2022-07-08 01:22:57 +0300

    flow controller done, I think

--- controller/controller.v
+++ controller/controller.v
@@ -17,10 +17,10 @@ module controller(  output ACTIVE,
     assign ACTIVE = ACTIVE_TMP;
     assign MODE = MODE_TMP;
 
-    wire [2:0]tmp;
+    wire [2:0]ignore;
     
-    DEC_INPUT_KEY DRM(ACTIVE_TMP,MODE_TMP,INPUT_KEY,VALID_CMD,RESET,CLK,tmp);
-    CONTROL_RW_FLOW CTRL(ACCESS_MEM,RW_MEM,PARALLEL_LOAD,Tx_DATA,BUSY,ACTIVE_TMP,MODE_TMP,VALID_CMD,RW,Tx_DONE,RESET,CLK);
+    DEC_INPUT_KEY DRM(ACTIVE_TMP,MODE_TMP,INPUT_KEY,VALID_CMD,RESET,CLK,ignore);
+    CONTROL_RW_FLOW CTRL(ACCESS_MEM,RW_MEM,PARALLEL_LOAD,Tx_DATA,BUSY,ACTIVE_TMP,MODE_TMP,VALID_CMD,RW,Tx_DONE,RESET,CLK,ignore);
 endmodule
 
 module DEC_INPUT_KEY(   output reg ACTIVE,
@@ -74,7 +74,8 @@ module Clock(output c);
     #5 value = ~value;
 endmodule
 
-module tester();
+/*
+module tester_DEC_INPUT_KEY();
     wire ACTIVE;
     wire MODE;
     reg  INPUT_KEY;
@@ -133,7 +134,7 @@ module tester();
 #20     $finish();
     end
 endmodule
-
+*/
 
 module CONTROL_RW_FLOW( output reg ACCESS_MEM,
                         output reg RW_MEM,
@@ -146,7 +147,111 @@ module CONTROL_RW_FLOW( output reg ACCESS_MEM,
                         input      RW,
                         input      Tx_DONE,
                         input      RESET,
-                        input      CLK);
+                        input      CLK,
+                        output [2:0] DEBUG);
+    reg [2:0] state;
+    reg [2:0] next_state;
+
+    assign DEBUG = next_state;
+    
+    always @(posedge CLK or posedge RESET)
+    begin
+        if(RESET)
+            state <= 0;
+        else
+            state <= next_state;
+        
+    end
+
+    always @(state, ACTIVE,MODE,VALID_CMD,RW,Tx_DONE)
+    begin
+        case(state)
+            0:begin
+                if(VALID_CMD == 1 && RW == 0 && ACTIVE == 1 && MODE == 1 && Tx_DONE == 1) next_state <= 1;
+                else if(VALID_CMD == 1 && RW == 1 && ACTIVE == 1 && MODE == 1) next_state <= 4;
+                else if(VALID_CMD == 1 && ACTIVE == 1 && MODE == 0 && Tx_DONE == 1) next_state <= 5;
+                else next_state <= 0;
+            end
+            1:begin
+                if(ACTIVE == 1 && MODE == 1 && Tx_DONE == 1) next_state <= 2;
+                else next_state <= 0;
+            end
+            2:begin
+                if(ACTIVE == 1 && MODE == 1) next_state <= 3;
+                else next_state <= 0;
+            end
+            3:begin
+                if(ACTIVE == 1 && MODE == 1 && Tx_DONE == 0) next_state <= 3;
+                else next_state <= 0;
+            end
+            4:begin
+                next_state <= 0;
+            end
+            5:begin
+                if(ACTIVE == 1 && MODE == 0) next_state <= 6;
+                else next_state <= 0;
+            end
+            6:begin
+                if(ACTIVE == 1 && MODE == 0 && Tx_DONE == 0) next_state <= 6;
+                else next_state <= 0;
+            end
+        endcase
+    end
+
+    always @(state)
+    begin
+        case(state)
+            0:begin
+                ACCESS_MEM <= 0;
+                RW_MEM <= 0;
+                PARALLEL_LOAD <= 0;
+                Tx_DATA <= 0;
+                BUSY <= 0;
+            end
+            1:begin
+                ACCESS_MEM <= 1;
+                RW_MEM <= 0;
+                PARALLEL_LOAD <= 0;
+                Tx_DATA <= 0;
+                BUSY <= 1;
+            end
+            2:begin
+                ACCESS_MEM <= 0;
+                RW_MEM <= 0;
+                PARALLEL_LOAD <= 1;
+                Tx_DATA <= 1;
+                BUSY <= 1;
+            end
+            3:begin
+                ACCESS_MEM <= 0;
+                RW_MEM <= 0;
+                PARALLEL_LOAD <= 0;
+                Tx_DATA <= 0;
+                BUSY <= 1;
+            end
+            4:begin
+                ACCESS_MEM <= 1;
+                RW_MEM <= 1;
+                PARALLEL_LOAD <= 0;
+                Tx_DATA <= 0;
+                BUSY <= 0;
+            end
+            5:begin
+                ACCESS_MEM <= 0;
+                RW_MEM <= 0;
+                PARALLEL_LOAD <= 1;
+                Tx_DATA <= 1;
+                BUSY <= 1;
+            end
+            6:begin
+                ACCESS_MEM <= 0;
+                RW_MEM <= 0;
+                PARALLEL_LOAD <= 0;
+                Tx_DATA <= 0;
+                BUSY <= 1;
+            end
+        endcase
+    end
 endmodule
 
 module D_FF(output reg Q,
@@ -156,5 +261,41 @@ module D_FF(output reg Q,
     begin
         Q <= D;
     end
+endmodule
+
+module tester();
+    wire ACCESS_MEM;
+    wire RW_MEM;
+    wire PARALLEL_LOAD;
+    wire Tx_DATA;
+    wire BUSY;
+    reg ACTIVE;
+    reg MODE;
+    reg VALID_CMD;
+    reg RW;
+    reg Tx_DONE;
+    reg RESET;
+    wire CLK;
+    Clock clk(CLK);
+    wire [2:0]STATE;
+    CONTROL_RW_FLOW DUT(ACCESS_MEM,RW_MEM,PARALLEL_LOAD,Tx_DATA,BUSY,ACTIVE,MODE,VALID_CMD,RW,Tx_DONE,RESET,CLK,STATE);
+    
+    initial
+    begin
+        $dumpfile("flow.vcd");
+        $dumpvars(0,ACCESS_MEM,RW_MEM,PARALLEL_LOAD,Tx_DATA,BUSY,ACTIVE,MODE,VALID_CMD,RW,Tx_DONE,RESET,CLK,STATE);
+        RESET = 1;
+        ACTIVE = 1;
+        MODE = 0;
+        VALID_CMD = 0;
+        RW = 0;
+        Tx_DONE = 0;
+
+
+#5      RESET = 0;
+
+#20     $finish();
+    end
+
 endmodule
 
